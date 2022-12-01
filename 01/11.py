b8838f853faed1146bbf895c6deca54a20c583f9
Mattias Ahle <mattias.ahle@gmail.com>
2022-12-01 07:15:19 +0100

    1.1 finished

---
+++ 01/11.py
@@ -0,0 +1,17 @@
+# Read input
+with open("input.txt", "r") as f:
+    input = f.readlines()
+
+sum = 0
+max_sum = 0
+
+for line in input:
+    if line == '\n':
+        if sum > max_sum:
+            max_sum = sum
+        sum = 0
+        continue
+    else:
+        sum += int(line)
+
+print(max_sum)
