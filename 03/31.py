ef982c8769ceaa0f22a7b9b10e2424beeaaeed93
Mattias Ahle <mattias.ahle@gmail.com>
2022-12-03 08:09:02 +0100

    3.1 finished

--- 03/31.py
+++ 03/31.py
@@ -9,10 +9,46 @@ def parse_input(input):
     return parsed_input
 
 
+def print_input(input):
+    for line in range(len(input)):
+        print(input[line])
+
+
+def calculate_priority(char):
+    if char.islower():
+        return ord(char) - 96
+    else:
+        return ord(char) - 38
+
+
+def split_content(content):
+    compartment1 = content[0:len(content)//2]
+    compartment2 = content[len(content)//2:len(content)]
+
+    return compartment1, compartment2
+
+
+def get_common_item(compartment1, compartment2):
+    for item in compartment1:
+        if item in compartment2:
+            return item
+
+
+def get_priority(content):
+    compartment1, compartment2 = split_content(content)
+    common_item = get_common_item(compartment1, compartment2)
+
+    return calculate_priority(common_item)
+
+
 def main():
-    input = read_input()
-    input = parse_input(read_input())
-    
+    sum_of_priorities = 0
+    contents = parse_input(read_input())
+
+    for content in contents:
+        sum_of_priorities += get_priority(content)
+
+    print(sum_of_priorities)
 
 
 if __name__ == '__main__':
