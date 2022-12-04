abd951d2bf436196cb942082232f44bd98aa8dbf
Mattias Ahle <mattias.ahle@gmail.com>
2022-12-04 21:31:41 +0100

    Correct answer 4.1

--- 04/day41.py
+++ 04/day41.py
@@ -3,16 +3,53 @@ def read_input():
         return f.readlines()
 
 
-def parse_input(input):
-    parsed_input = [line.replace('\n', '') for line in input]
+def parse_input(input: list):
+    input = [line.replace('\n', '') for line in input]
+    input = [line.split(',') for line in input]
 
-    return parsed_input
+    return input
+
+
+def is_in_range(number: int, range_low: int, range_high: int):
+    return range_low <= number <= range_high
+
+
+def get_first_low(assigment: list):
+    return int(assigment[0].split('-')[0])
+
+
+def get_first_high(assigment: list):
+    return int(assigment[0].split('-')[1])
+
+
+def get_second_low(assigment: list):
+    return int(assigment[1].split('-')[0])
+
+
+def get_second_high(assigment: list):
+    return int(assigment[1].split('-')[1])
+
+
+def is_in_full_range(a):
+    if (is_in_range(get_first_low(a), get_second_low(a), get_second_high(a)) and 
+        is_in_range(get_first_high(a), get_second_low(a), get_second_high(a))):
+        return True
+    elif (is_in_range(get_second_low(a), get_first_low(a), get_first_high(a)) and 
+        is_in_range(get_second_high(a), get_first_low(a), get_first_high(a))):
+        return True
+    else:
+        return False
 
 
 def main():
-    input = read_input()
-    input = parse_input(read_input())
+    number_of_assignments = 0
+    assignments = parse_input(read_input())
     
+    for assignment in assignments:
+        if(is_in_full_range(assignment)):
+            number_of_assignments += 1
+
+    print(number_of_assignments)
 
 
 if __name__ == '__main__':
