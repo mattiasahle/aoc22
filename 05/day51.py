423c478ff2fdf1bb14cf1f85ac094e9b7888d644
Mattias Ahle <mattias.ahle@gmail.com>
2022-12-05 14:16:50 +0100

    Progress...

--- 05/day51.py
+++ 05/day51.py
@@ -1,3 +1,6 @@
+import re
+
+
 def read_input():
     with open("input.txt", "r") as f:
         return f.readlines()
@@ -20,12 +23,38 @@ def get_stacks(input):
     return stacks
 
 
+def get_moves(input):
+    moves = []
+
+    for i in range(10, len(input)):
+        moves.append(re.findall('\d+', input[i]))
+
+    return moves
+
+
+def make_move(move, stacks):
+    pops = int(move[0])
+    move_from = int(move[1])
+    move_to = int(move[2])
+
+    for _ in range(pops):
+        popped = stacks[move_from].pop()
+        stacks[move_to].append(popped)
+
+    return stacks
+
+
 def main():
     input = parse_input(read_input())
     stacks = get_stacks(input)
+    moves = get_moves(input)
 
-    for stack in stacks:
-        print(stack)
+    print(moves[0][0])
+    print(moves[0][1])
+    print(moves[0][2])
+
+    # for move in moves:
+    #     stacks = make_move(move, stacks)
 
 
 if __name__ == '__main__':
