77c9c3e73b208bc58614a89bef768de48a08d02d
Mattias Ahle <mattiaah@axis.com>
2022-12-08 08:17:58 +0100

    Correct answer 8.1
    Change-Id: I7c1a6c2050f50eaf4041e3c6d57d2f6b7f317e41

--- 08/day81.py
+++ 08/day81.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 def read_input():
     with open("input.txt", "r") as f:
         return f.readlines()
@@ -5,13 +8,81 @@ def read_input():
 
 def parse_input(input):
     parsed_input = [line.rstrip('\n') for line in input]
+    parsed_input = np.array([[char for char in line]
+                            for line in parsed_input], np.int8)
 
     return parsed_input
 
 
+def is_visible_north(input, row, column):
+    tree_height = input[row][column]
+
+    while row >= 1:
+        row -= 1
+        if input[row][column] >= tree_height:
+            return False
+
+    return True
+
+
+def is_visible_south(input, row, column):
+    tree_height = input[row][column]
+
+    while row <= 97:
+        row += 1
+        if input[row][column] >= tree_height:
+            return False
+
+    return True
+
+
+def is_visible_west(input, row, column):
+    tree_height = input[row][column]
+
+    while column >= 1:
+        column -= 1
+        if input[row][column] >= tree_height:
+            return False
+
+    return True
+
+
+def is_visible_east(input, row, column):
+    tree_height = input[row][column]
+
+    while column <= 97:
+        column += 1
+        if input[row][column] >= tree_height:
+            return False
+
+    return True
+
+
+def get_num_visible_trees(input):
+    visible_trees = input.shape[0] * 4 - 4  # Add outer trees
+
+    for row in range(1, len(input[:-1])):
+        for column in range(1, len(input[row][:-1])):
+            if is_visible_north(input, row, column):
+                visible_trees += 1
+            elif is_visible_south(input, row, column):
+                visible_trees += 1
+            elif is_visible_west(input, row, column):
+                visible_trees += 1
+            elif is_visible_east(input, row, column):
+                visible_trees += 1
+
+    return visible_trees
+
+
 def main():
     input = parse_input(read_input())
+    print(get_num_visible_trees(input))
 
 
 if __name__ == '__main__':
     main()
+
+
+# Too high: 9409, 1737
+# Too low: 1341
